4e79eb339bdc5858e06c959cf39b7887e8259394
Benzel <benawhiting@gmail.com>
2024-06-01 13:57:13 -0700

    Select a word upon initialization

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,19 @@
 class Game
   attr_accessor :word
 
-  def initialize; end
+  def initialize
+    self.word = choose_word
+  end
 
   private
 
   def choose_word
     file = File.open('public/google-10000-english-no-swears.txt')
-    file.readlines.map(&:chomp).select { |word| word.length >= 5 && word.length <= 12 }
+    word_list = file.readlines.map(&:chomp).select { |word| word.length >= 5 && word.length <= 12 }
+    word_list[rand(0..word_list.length - 1)]
   end
 end
 
 game = Game.new
 
-game.choose_word
+puts game.word
